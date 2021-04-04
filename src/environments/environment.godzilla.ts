477c7bb7a3346443672a66e346773498fa297183
鈴木泰斗 <suzukitaito@suzukitaitonoMacBook-Pro.local>
2021-04-05 00:38:27 +0900

    save

---
+++ src/environments/environment.godzilla.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    api: {
+        baseUrl: 'https://api-blog.tach.dev',
+    },
+    storage: {
+        article: {
+            baseUrl: 'https://suzuito-godzilla-blog1-article.storage.googleapis.com',
+        },
+    },
+    production: true
+};
