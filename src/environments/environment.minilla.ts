c8fc10f1c2670a0d22d7112dc11cfd4d460e71f0
suzukitaito <suzukitaito@suzukitaitonoMacBook-Pro.local>
2022-02-06 23:06:27 +0900

    save

--- src/environments/environment.minilla.ts
+++ src/environments/environment.minilla.ts
@@ -2,7 +2,7 @@ import { Env } from './common';
 
 export const environment: Env = {
     api: {
-        baseUrl: 'https://minilla-api-blog.tach.dev',
+        baseUrl: 'https://api-blog.minilla.tach.dev',
     },
     storage: {
         article: {
